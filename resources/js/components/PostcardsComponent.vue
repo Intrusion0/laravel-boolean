ab1bfec6cf2a42866416a61061625332ddfdff9b
Mario <lombardomario01@gmail.com>
2022-02-15 12:49:11 +0100

    Component "Postcards" added

---
+++ resources/js/components/PostcardsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container-postcards">
+        <table border="1">
+            <tr>
+                <th>a</th>
+                <th>b</th>
+                <th>c</th>
+                <th>d</th>
+            </tr>
+            <tr>
+                <td>a</td>
+                <td>a</td>
+                <td>a</td>
+                <td>a</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Componente')
+        }
+    }
+</script>
